6369b324c83f31aeafc8ee0c7630b972f87eff8b
Santiago Gomez Cornejo <131918914+Santos2705@users.noreply.github.com>
2025-02-14 19:16:49 -0400

    Arreglo responsive

--- script.js
+++ script.js
@@ -161,6 +161,22 @@ window.onload = function() {
     }
 };
 
+@media screen and (min-wdth:400px) {
+    body{
+        background-color: none;
+    }
+
+
+}
+
+
+@media screen and (min-width:800px){
+    body{
+
+        background-color: none;
+    }
+}
+
 
 
 
